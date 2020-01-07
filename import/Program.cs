31402f51420556cebd48358b1bf62a06bcf2d71c
ravndalf <fredrik.ravndal@allegro.no>
2020-01-07 09:36:11 +0100

    created fresh import job

---
+++ import/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
