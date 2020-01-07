c5fefa67759c90f8ef18bbcfff8a372dc4b14eab
ravndalf <fredrik.ravndal@allegro.no>
2020-01-07 16:26:04 +0100

    create migration

--- database/CSGOContext.cs
+++ database/CSGOContext.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using entity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace database
 {
-    class CSGOContext : DbContext
+    public class CSGOContext : DbContext
     {
+        public DbSet<Weapon> Weapons { get; set; }
+
+        //public CSGOContext(DbContextOptions<CSGOContext> options) : base(options)
+        //{
+            
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=localhost;Initial Catalog=csgo;Integrated Security=True;");
+        }
+
+        #region Required
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasSequence<int>("seq", schema: "csgo").StartsAt(0).IncrementsBy(1);
+            modelBuilder.Entity<Weapon>().HasKey(sc => new { sc.Id });
+        }
+        #endregion
     }
 }
---
+++ database/Migrations/20200107132051_initcreate.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace database.Migrations
+{
+    public partial class initcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "csgo");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "seq",
+                schema: "csgo",
+                startValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "Weapons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WeaponName = table.Column<string>(nullable: true),
+                    WeaponType = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    KillAward = table.Column<int>(nullable: false),
+                    Damage = table.Column<int>(nullable: false),
+                    Bullets = table.Column<int>(nullable: false),
+                    ArmorPenetration = table.Column<double>(nullable: false),
+                    DamageFalloff = table.Column<string>(nullable: true),
+                    FireRate = table.Column<double>(nullable: false),
+                    PenetrationPower = table.Column<string>(nullable: true),
+                    MagazineSize = table.Column<int>(nullable: false),
+                    AmmoReserve = table.Column<int>(nullable: false),
+                    Mobility = table.Column<int>(nullable: false),
+                    TaggingPower = table.Column<string>(nullable: true),
+                    BulletRange = table.Column<int>(nullable: false),
+                    HoldToShoot = table.Column<string>(nullable: true),
+                    Tracers = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weapons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weapons");
+
+            migrationBuilder.DropSequence(
+                name: "seq",
+                schema: "csgo");
+        }
+    }
+}
