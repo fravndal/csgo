8294d9465277ad41d9707396c9da2754beb87750
ravndalf <fredrik.ravndal@allegro.no>
2020-01-16 13:55:48 +0100

    refactor repo

---
+++ Core/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Interfaces
+{
+    using Core.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public interface IRepository
+    {
+        T GetById<T>(int id) where T : BaseEntity;
+        List<T> List<T>() where T : BaseEntity;
+        void Add<T>(T entity) where T : BaseEntity;
+    }
+}
--- Infrastructure/SQLite/Repositories/DbRepository.cs
+++ Infrastructure/SQLite/Repositories/DbRepository.cs
@@ -2,26 +2,33 @@
 {
     using Core.Entities;
     using Core.Interfaces;
-    using System.Linq;
     using System.Collections.Generic;
+    using System.Linq;
 
-    public class WeaponRepository : IWeaponRepository
+    public class DbRepository : IRepository
     {
         private readonly CSGOContext _dbContext;
+        
 
-        public WeaponRepository(CSGOContext dbContext)
+        public DbRepository(CSGOContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public T GetWeapon<T>(int id) where T : BaseEntity
+        public T GetById<T>(int id) where T : BaseEntity
         {
             return _dbContext.Set<T>().SingleOrDefault(e => e.Id == id);
         }
 
-        public List<T> GetWeapons<T>() where T : BaseEntity
+        public List<T> List<T>() where T : BaseEntity
         {
             return _dbContext.Set<T>().ToList();
+        }
+
+        public void Add<T>(T entity) where T : BaseEntity
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
         }
     }
 }
